59b736962fd6a7bfc556a8d37ff83987a11c06eb
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-02-13 01:38:34 +0600

    Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -1,13 +1,22 @@
 <?php
 
 $heading = "My Notes";
-
+$currentUserId = 2;
 
 $config = require("config.php");
 $db = new Database($config['database']);
 
 
-$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();;
+$note = $db->query(
+  "select * from notes where id = :id",
+  ['id' => $_GET['id']]
+)->fetch();
 
+if (!$note) {
+  abort(Response::NOT_FOUND);
+}
+if ($note['user_id'] !== $currentUserId) {
+  abort(Response::FORBDDEN);
+}
 
 require "views/note.view.php";
--- controllers/notes.php
+++ controllers/notes.php
@@ -3,11 +3,11 @@
 
 $config = require("config.php");
 $db = new Database($config['database']);
+$currentUserId = 2;
 
+$heading = $db->query("select * from users where id={$currentUserId}")->fetch()['name'] . " Notes";
 
-$heading = $db->query("select * from users where id=2")->fetch()['name'] . " Notes";
-
-$query = "select * from notes where user_id = 2";
+$query = "select * from notes where user_id = {$currentUserId}";
 
 // $id = $_GET['id'];
 $notes = $db->query($query)->fetchAll();;
