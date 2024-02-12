7a920eb43a21bdd3426dd4dc127bce68cdf9fc72
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-02-12 21:33:56 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require("functions.php");
+require("Database.php");
+
+// require("router.php");
+
+
+$config = require("config.php");
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+dd($posts);
+
+// foreach ($posts as $post) {
+//   echo "<li>{$post['title']}</li>";
+// }
