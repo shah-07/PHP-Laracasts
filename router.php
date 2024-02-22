730cfbff1e87b3ab2ce406a176a96d49466b9c92
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-02-22 13:48:22 +0600

    create Forms and Request Methods

--- router.php
+++ router.php
@@ -1,14 +1,8 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-$routes = [
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/notes' => 'controllers/notes.php',
-  '/note' => 'controllers/note.php',
-  '/contact' => 'controllers/contact.php',
-];
+$routes = require("routes.php");
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 function routeToController($uri, $routes)
 {
   if (array_key_exists($uri, $routes)) {
--- views/notes.view.php
+++ views/notes.view.php
@@ -6,13 +6,16 @@
 
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    <?php foreach ($notes as $note) : ?>
-      <li>
-        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-          <?= $note['body'] ?>
-        </a>
-      </li>
-    <?php endforeach; ?>
+    <ul>
+      <?php foreach ($notes as $note) : ?>
+        <li>
+          <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+            <?= $note['body'] ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+    <a type="button" class="mt-6 underline" href="/notes/create">Creat A Note</a>
   </div>
 </main>
 
