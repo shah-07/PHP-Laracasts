39cec1d88135f0b830431b4965b253ba4e1e815e
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-03-10 12:46:26 +0600

    Build a Better Router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -12,6 +12,7 @@
     <p><?= htmlspecialchars($note['body']) ?></p>
 
     <form class="mt-6" method="post">
+      <input type="hidden" name="_method" value="DELETE">
       <input type="hidden" name="id" value="<?= $note['id'] ?>">
       <button type="submit" class="text-sm text-red-500">Delete</button>
     </form>
