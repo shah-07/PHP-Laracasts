d2aa6f4e8acc760f2777381b93b0346a4a0f0c29
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-03-12 20:00:20 +0600

    Updating With PATCH Requests

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -9,13 +9,20 @@
     <p class="mb-6">
       <a href="/notes" class="text-blue-500 underline">go back...</a>
     </p>
-    <p><?= htmlspecialchars($note['body']) ?></p>
+    <p>
+      <?= htmlspecialchars($note['body']) ?>
+    </p>
+    <footer class="flex space-x-4 items-center mt-6">
+      <a class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+        href="note/edit?id=<?= $note['id'] ?>">Edit</a>
+      <form method="post">
+        <input type="hidden" name="_method" value="DELETE">
+        <input type="hidden" name="id" value="<?= $note['id'] ?>">
+        <button type="submit"
+          class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+      </form>
+    </footer>
 
-    <form class="mt-6" method="post">
-      <input type="hidden" name="_method" value="DELETE">
-      <input type="hidden" name="id" value="<?= $note['id'] ?>">
-      <button type="submit" class="text-sm text-red-500">Delete</button>
-    </form>
   </div>
 </main>
 
